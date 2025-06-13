b27e1c90c07e7dc4fe166fc2eb5663d50b46c1b1
Ieuan Byers <ieuan.byers@justice.gov.uk>
2025-06-13 10:01:37 +0100

    Add pub ip and gatway subnet to module

---
+++ networking.tf
@@ -0,0 +1,13 @@
+resource "azurerm_subnet" "gateway_subnet" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.virtual_network_name
+  address_prefixes     = var.address_prefixes
+}
+
+resource "azurerm_public_ip" "gateway_ip" {
+  name                = var.public_ip_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = var.public_ip_allocation_method
+}
