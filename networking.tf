e74776605fda71b91684b1c9657f0fdff74ae7f2
Ieuan Byers <ieuan.byers@justice.gov.uk>
2025-06-13 10:18:05 +0100

    Add count to resources

--- networking.tf
+++ networking.tf
@@ -1,4 +1,6 @@
 resource "azurerm_subnet" "gateway_subnet" {
+  count = var.create_gateway_subnet ? 1 : 0
+
   name                 = var.subnet_name
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
@@ -6,6 +8,8 @@ resource "azurerm_subnet" "gateway_subnet" {
 }
 
 resource "azurerm_public_ip" "gateway_ip" {
+  count = var.create_public_ip ? 1 : 0
+
   name                = var.public_ip_name
   location            = var.location
   resource_group_name = var.resource_group_name
